e7564bef4d2598fc987288174531c3e1416d3788
Koltovich_Vasil <Vasil_Koltovich@epam.com>
2018-07-10 11:37:46 +0300

    Update

--- src/main/java/util/PdfView.java
+++ src/main/java/util/PdfView.java
@@ -21,22 +21,30 @@ public class PdfView extends AbstractPdfView {
                                     HttpServletResponse response) throws Exception {
         List<Ticket> ticketList = (List<Ticket>) model.get("tickets");
 
-        PdfPTable table = new PdfPTable(3);
+        PdfPTable table = new PdfPTable(5);
         table.setWidthPercentage(60);
-        table.setWidths(new int[] {1, 3, 3});
+        table.setWidths(new int[] {3, 4, 3,3,3});
 
         Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
 
         PdfPCell hcell;
-        hcell = new PdfPCell(new Phrase("Id", headFont));
+        hcell = new PdfPCell(new Phrase("Event", headFont));
         hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(hcell);
 
-        hcell = new PdfPCell(new Phrase("Name", headFont));
+        hcell = new PdfPCell(new Phrase("Auditorium", headFont));
         hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(hcell);
 
         hcell = new PdfPCell(new Phrase("Price", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Seats", headFont));
+        hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(hcell);
+
+        hcell = new PdfPCell(new Phrase("Date", headFont));
         hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
         table.addCell(hcell);
 
@@ -50,6 +58,18 @@ public class PdfView extends AbstractPdfView {
             table.addCell(cell);
 
             cell = new PdfPCell(new Phrase(ticket.getEvent().getAuditorium().getName()));
+            cell.setPaddingLeft(5);
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(Double.toString(ticket.getPrice())));
+            cell.setPaddingLeft(5);
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+            table.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(ticket.getSeats()));
             cell.setPaddingLeft(5);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_LEFT);
