99e1d0ff516a4a0e04d20d7de99058399861983f
Koltovich_Vasil <Vasil_Koltovich@epam.com>
2018-07-25 10:19:01 +0300

    Update

--- src/main/java/beans/daos/UserAccountRepository.java
+++ src/main/java/beans/daos/UserAccountRepository.java
@@ -2,13 +2,15 @@ package beans.daos;
 
 import beans.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(rollbackFor = Exception.class)
-public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+//public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+public interface UserAccountRepository extends SimpleJpaRepository<UserAccount, Long> {
 
     UserAccount findByUser_email(String email);
     UserAccount findByUser_name(String name);
