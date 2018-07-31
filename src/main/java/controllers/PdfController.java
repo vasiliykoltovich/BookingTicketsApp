892c031a765c4b2214047a13e9a3212f08f2be83
djedi <vasiliy.koltovich@gmail.com>
2018-07-31 22:21:19 +0300

    SOAP

--- src/main/java/controllers/PdfController.java
+++ src/main/java/controllers/PdfController.java
@@ -6,14 +6,12 @@ import beans.models.soap.User;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import util.PdfView;
 
@@ -21,11 +19,13 @@ import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Controller
+//@Controller
+@RestController
 public class PdfController extends GenericController {
 
     @PreAuthorize("hasAuthority('BOOKING_MANAGER')")
-    @GetMapping(value = "/getTicketForEvent", produces = MediaType.APPLICATION_PDF_VALUE,params = {"event","auditorium","date"},headers = {"accept"})
+    @GetMapping(value = "/getTicketForEvent", produces = MediaType.APPLICATION_PDF_VALUE,params = {"event","auditorium","date"},
+            headers = {"accept"})
     public ModelAndView getTicketForEvent(@Nullable @RequestHeader("accept") String accept, @RequestParam("event") String eventName,
                                           @RequestParam("auditorium") String auditorium,
                                           @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date) {
@@ -45,6 +45,7 @@ public class PdfController extends GenericController {
 
     }
 
+
     @GetMapping(value = "/getBookedTicketsByUser", produces = MediaType.APPLICATION_PDF_VALUE,params = {"email"},headers = {"accept"})
     @PreAuthorize("hasAuthority('BOOKING_MANAGER')")
     public ModelAndView getBookedTicketsByUser(@Nullable @RequestHeader("accept") String accept, @RequestParam("email") String email) {
@@ -61,6 +62,32 @@ public class PdfController extends GenericController {
 
         return view;
     }
+
+    @PostMapping(value = "/bookTicket",headers = {"accept","content-type"},
+            params = {"email","event","auditorium","date","seats"},
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyAuthority('REGISTERED_USER','BOOKING_MANAGER')")
+    public ResponseEntity<Ticket> bookTicket(Model model, @Nullable @RequestHeader("accept") String accept,
+                             @Nullable @RequestHeader("content-type") String contentType,
+                             @RequestParam("email") String email,
+                             @RequestParam("event") String eventName, @RequestParam("auditorium") String auditorium,
+                             @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date,
+                             @RequestParam("seats") String seats) {
+
+        User user = userService.getUserByEmail(email);
+        List<Integer> seatList = Arrays.asList(seats.split(",")).stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        Event event = eventService.getEvent(eventName, auditoriumService.getByName(auditorium), date);
+        double eventPrice = bookingService.getTicketPrice(event.getName(), event.getAuditorium().getName(), event.getDateTime(), seatList, user);
+        Ticket booked = bookingService.bookAndReturnTicket(user, new Ticket(event, LocalDateTime.now(), seatList, user, eventPrice));
+//       return tickets;
+//        model.
+        return new ResponseEntity<>(booked,HttpStatus.CREATED);
+
+    }
+
+
+
 
 
 
@@ -91,5 +118,6 @@ public class PdfController extends GenericController {
         return view;
 
     }
+
 
 }
